89bf753328d25a019759c70c9e201ad4a773c01d
imgs <22imgs21212222@gmail.com>
2023-08-01 01:26:58 -0400

    Started enemy movement
    - Need to properly implement a system to combine movement with shooting (that's why shooting is disabled rn)

--- Boss Summer Project/Assets/Scripts/Enemy/Enemy.cs
+++ Boss Summer Project/Assets/Scripts/Enemy/Enemy.cs
@@ -1,60 +1,76 @@
+/* (Even though there are a lot of similarities between this and the player 
+controller, I don't feel like doing any abstraction cause it would be too much effort - kevin) */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float attackRadius;
-    public float fireRate;
-
-    public static bool ProjectileTargeting;
-    
-    public GameObject particles;
-
-    [SerializeField] private Vector3 spawnPoint;
-
-    private bool canCollide = true;
-    public bool CanCollide {
-        get{return canCollide;}
-        set{canCollide = value;}
-    }
+    [Header("Projectile Firing")]
+    [SerializeField] private float fireRate;
     public GameObject Projectile;
 
-    public Transform player;
+    [Header("Enemy Movement")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float gravityCoefficient; // If the enemy is in a reverse gravity area 
 
-    [SerializeField] private bool canShoot;
-
+    [Header("Collision Checking - Layer Masks")]
     [SerializeField] private LayerMask whatIsPlayer;
+    [SerializeField] private LayerMask whatIsGround;
+
+    [Header("Dying")]
     public GameObject deathEffect; 
     private PlayerController playerController;
     private Rigidbody2D rb2d;
     private BoxCollider2D bc2d;
+    private bool canShoot;
+    private bool isGrounded;
+
+    // private bool canCollide = true;
+    // public bool CanCollide {
+    //     get{return canCollide;}
+    //     set{canCollide = value;}
+    // }
+
+    // Why do we need canCollide here - Kevin
+
+    private float direction;
 
     // Start is called before the first frame update
     void Start()
     {
-        canCollide = true;
+        // canCollide = true;
         rb2d = GetComponent<Rigidbody2D>();
         bc2d = GetComponent<BoxCollider2D>();
         playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         canShoot = true;
-        ProjectileTargeting = false;
+
+        direction = 1f;
     }
     // Called every fixed timestep
     // Used for physics
     void FixedUpdate()
     {
-        Vector2 dir = player.position - transform.position;
+        // Vector2 dir = player.position - transform.position; 
 
-        if(dir.sqrMagnitude <= attackRadius && canShoot){
-            StartCoroutine(shoot());
-        }
+        // if(dir.sqrMagnitude <= attackRadius && canShoot){
+        //     StartCoroutine(shoot());
+        // }
+
+        // Temporarily disable the shooting so that I can easily work on the movement - kevin
+
+        if(isGrounded)
+            rb2d.velocity = new Vector2(direction * moveSpeed * 100f * Time.fixedDeltaTime, rb2d.velocity.y);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         bool collideWithPlayer = Physics2D.BoxCast(transform.position, new Vector2(0.45f, 0.45f), 0f, Vector2.down, 0f, whatIsPlayer);
+        isGrounded = Physics2D.BoxCast(transform.position, new Vector2(0.45f - 0.1f, 0.45f - 0.1f), 0f, gravityCoefficient * Vector2.down, 0.1f, whatIsGround);
 
         if(collideWithPlayer)
         {
@@ -67,7 +83,7 @@ public class Enemy : MonoBehaviour
 
     void die()
     {
-        GameObject deathParticles = Instantiate(deathEffect);
+        GameObject deathParticles = Instantiate(deathEffect);   
         deathParticles.transform.position = transform.position;
 
         StopAllCoroutines();
@@ -77,19 +93,31 @@ public class Enemy : MonoBehaviour
         ParticleSystem.MainModule dpMain = dpSystem.main;
 
         dpMain.startColor = gameObject.GetComponent<SpriteRenderer>().color;
+
+        Destroy(deathParticles, 2f);
     }
-    //This method will run when the player collides with something
+
+    //This method will run when the player collides with something (THIS IS THE ENEMY SCRIPT??? LOL)
     void OnCollisionEnter2D(Collision2D col) {
         if(col.gameObject.tag == "Kill")
         {
             die();
         }
 
+        if(col.gameObject.tag == "DirectionChange")
+        {
+            direction *= -1;
+        }
+
     }
-    void respawn(){
-        gameObject.SetActive(true);
-        transform.position = spawnPoint;
-    }
+
+    // void respawn(){
+    //     gameObject.SetActive(true);
+    //     transform.position = spawnPoint;
+    // }  
+
+    // Commented this out since I don't think enemies should respawn - kevin
+
     IEnumerator shoot()
     {
         canShoot = false;
